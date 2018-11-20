ab735e6e2f36bde157ff2ce565b847949934e5c0
Maxim Tsvetkov <777.maxxx@gmail.com>
2018-11-20 22:45:29 +0100

    Add list service tests

---
+++ RatedList/RatedListTests/ListServiceTests.swift
@@ -0,0 +1,30 @@
+//
+//  ListServiceTests.swift
+//  RatedListTests
+//
+//  Created by MAXIM TSVETKOV on 20.11.18.
+//  Copyright © 2018 MAXIM TSVETKOV. All rights reserved.
+//
+
+import XCTest
+@testable import RatedList
+
+class ListServiceTests: XCTestCase {
+
+    let service = ListServiceImp()
+
+    func testServiceFetchesItemsCorrectly() {
+        
+        //Given
+        let exp = expectation(description: "service fetches items")
+        
+        //When
+        service.fetchItems { items in
+            //Then
+            XCTAssertEqual(items?.count, 18, "ListService does not fetch items correctly")
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 0.1)
+    }
+}
